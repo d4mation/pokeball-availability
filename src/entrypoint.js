3823c892da80c2c55e8ea5e95fca1521f572038a
Eric Defore <eric.defore@gmail.com>
2019-05-08 18:50:48 -0400

    Working webpack build environment

---
+++ src/entrypoint.js
@@ -0,0 +1,15 @@
+import $ from 'jquery';
+import whatInput from 'what-input';
+
+window.$ = $;
+
+//import Foundation from 'foundation-sites';
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+import './lib/foundation-explicit-pieces';
+
+$( document ).foundation();
+
+import './sass/app.scss';
+
+import FormContainer from "./js/components/container/FormContainer.jsx";
---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const path = require( 'path' );
+
+module.exports = {
+  entry: [
+    './src/entrypoint.js'
+  ],
+  output: {
+    filename: 'dist/js/app.js',
+    path: path.resolve( __dirname ),
+  },
+  devtool: 'source-map', // any "source-map"-like devtool is possible
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                // only enable hot in development
+                hmr: process.env.NODE_ENV === 'development',
+                sourceMap: true,
+              },
+            },
+            {
+              loader: "css-loader", // translates CSS into CommonJS
+              options: {
+                sourceMap: true,
+              }
+            },
+            {
+              loader: "sass-loader", // compiles Sass to CSS, using Node Sass by default
+              options: {
+                sourceMap: true,
+              }
+            }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin( {
+      template: "./index.html",
+      filename: "index.html",
+      inject: false
+    } ),
+    new MiniCssExtractPlugin( {
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'dist/css/app.css',
+      chunkFilename: 'dist/css/[id].css',
+    } )
+  ],
+  devServer: {
+    contentBase: './',
+    // 'Live-reloading' happens when you make changes to code
+    // dependency pointed to by 'entry' parameter explained earlier.
+    // To make live-reloading happen even when changes are made
+    // to the static html pages in 'contentBase', add 
+    // 'watchContentBase'
+    watchOptions: {
+        poll: true
+    }
+  }
+};
